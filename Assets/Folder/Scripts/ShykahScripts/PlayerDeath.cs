a8bcf243d58a0fa90bf5acb50dee1b596c0e45fa
yoon <yoonn808@gmail.com>
2025-10-21 13:13:48 +0300

    task done

---
+++ Assets/Folder/Scripts/ShykahScripts/PlayerDeath.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    public AudioClip deathSound;
+    private AudioSource audioSource;
+    private bool isDead = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Die()
+    {
+        if (isDead) return;
+
+        isDead = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(deathSound);
+
+        // تعطيل الحركة والتحكم
+        var cc = GetComponent<CharacterController>();
+        if (cc) cc.enabled = false;
+        // Use string-based lookup to avoid compile-time dependency on PlayerMovement
+        var pm = GetComponent("PlayerMovement") as MonoBehaviour;
+        if (pm != null) pm.enabled = false;
+
+
+        Debug.Log("Player Died!");
+    }
+}
