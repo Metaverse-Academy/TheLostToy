ec5d294d17f672b60afb35350c199f7edf366507
u-n79 <cooc.2010589@gmail.com>
2025-10-22 20:00:49 +0300

    a

--- Assets/Folder/Scripts/AliScripts/PlayerScripts/PlayerHealth.cs
+++ Assets/Folder/Scripts/AliScripts/PlayerScripts/PlayerHealth.cs
@@ -5,9 +5,11 @@ public class PlayerHealth : MonoBehaviour
     public int maxHealth = 100;
     public HeartbeatSound heartbeatSound;
     private int currentHealth;
+    private GameManager gameManager;
     void Start()
     {
         currentHealth = maxHealth;
+        gameManager = FindObjectOfType<GameManager>();
     }
 
     public void TakeDamage(int damage)
@@ -30,5 +32,9 @@ public class PlayerHealth : MonoBehaviour
     {
         Destroy(gameObject);
         Debug.Log("Player Died");
+        if (gameManager != null)
+        {
+            gameManager.GameOver();
+        }
     }
 }
