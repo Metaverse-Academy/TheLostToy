ec863986c1a7029f2390a2c0dabc1ce96583102c
abdullah-altedlawi <abode02651@gmail.com>
2025-10-23 17:25:38 +0300

    new deth

--- Assets/Folder/Scripts/AbdullahScripts/GameTimer.cs
+++ Assets/Folder/Scripts/AbdullahScripts/GameTimer.cs
@@ -10,6 +10,8 @@ public class GameTimer : MonoBehaviour
 
     [Header("UI Elements")]
     public TextMeshProUGUI timerText;
+    public GameManager gameManager;
+
 
     [Header("Color Settings")]
     public Color firstPeriodColor = Color.white;
@@ -36,6 +38,8 @@ public class GameTimer : MonoBehaviour
 
     void Update()
     {
+        GameEnd();
+
         if (isTimerRunning && remainingTime > 0)
         {
             remainingTime -= Time.deltaTime;
@@ -99,6 +103,13 @@ public class GameTimer : MonoBehaviour
         else
         {
             timerText.color = lastPeriodColor;
+        }
+    }
+    void GameEnd()
+    {
+        if (remainingTime <= 0)
+        {
+            gameManager.GameOver();
         }
     }
 }
