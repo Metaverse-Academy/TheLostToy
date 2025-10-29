943f86705e6cad211388b46793a7b5b6e48bbd3d
u-n79 <cooc.2010589@gmail.com>
2025-10-29 16:08:56 +0300

    A

---
+++ Assets/Folder/Scripts/AliScripts/Monday/GameManager2.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager2 : MonoBehaviour
+{
+    [SerializeField] private GameObject gameOverUI;
+    [SerializeField] private GameObject gameWinUI;
+    // [SerializeField] private GameTimer gameTimer;
+    public static GameManager2 Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void OnGameWin()
+    {
+        Time.timeScale = 0f;
+        gameWinUI.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void GameOver()
+    {
+        Time.timeScale = 0f;
+        gameOverUI.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        // gameTimer.remainingTime = gameTimer.timeDuration;
+    }
+}
---
+++ Assets/Folder/Scripts/AliScripts/Monday/PlayerHealth2.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerHealth2 : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public HeartbeatSound heartbeatSound;
+    private int currentHealth;
+    private GameManager gameManager;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Update()
+    {
+        if (heartbeatSound != null)
+        {
+            heartbeatSound.UpdateHeartbeat(currentHealth);
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+        Debug.Log("Player Died");
+        if (gameManager != null)
+        {
+            gameManager.GameOver();
+        }
+    }
+}
