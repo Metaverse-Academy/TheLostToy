5847a96c520cc2ec4adbe3a999468aeef1691ac2
u-n79 <cooc.2010589@gmail.com>
2025-10-29 16:47:00 +0300

    a
    This reverts commit 148f0f817c5709bf732461e17d7382fe2f2d1ea7, reversing
    changes made to a0dce6332e82944c72c49792ea6d4ada9481da3d.

---
+++ Assets/CodeMonkey/HealthSystem/Scripts/HealthBarUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CodeMonkey.HealthSystemCM {
+    public class HealthBarUI : MonoBehaviour {
+        [SerializeField] private GameObject getHealthSystemGameObject;
+        [SerializeField] private Image image;
+        private HealthSystem healthSystem;
+        private void Start() {
+            if (HealthSystem.TryGetHealthSystem(getHealthSystemGameObject, out HealthSystem healthSystem)) {
+                SetHealthSystem(healthSystem);
+            }
+        }
+        public void SetHealthSystem(HealthSystem healthSystem) {
+            if (this.healthSystem != null) {
+                this.healthSystem.OnHealthChanged -= HealthSystem_OnHealthChanged;
+            }
+            this.healthSystem = healthSystem;
+
+            UpdateHealthBar();
+
+            healthSystem.OnHealthChanged += HealthSystem_OnHealthChanged;
+        }
+        private void HealthSystem_OnHealthChanged(object sender, System.EventArgs e) {
+            UpdateHealthBar();
+        }
+        private void UpdateHealthBar() {
+            image.fillAmount = healthSystem.GetHealthNormalized();
+        }
+        private void OnDestroy() {
+            healthSystem.OnHealthChanged -= HealthSystem_OnHealthChanged;
+        }
+    }
+}
---
+++ Assets/CodeMonkey/HealthSystem/Scripts/HealthSystemComponent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace CodeMonkey.HealthSystemCM {
+    public class HealthSystemComponent : MonoBehaviour, IGetHealthSystem {
+
+        [SerializeField] private float healthAmountMax = 100f;
+        [SerializeField] private float startingHealthAmount;
+        private HealthSystem healthSystem;
+        private void Awake() {
+            healthSystem = new HealthSystem(healthAmountMax);
+            if (startingHealthAmount != 0) {
+                healthSystem.SetHealth(startingHealthAmount);
+            }
+        }
+        public HealthSystem GetHealthSystem() {
+            return healthSystem;
+        }
+    }
+}
