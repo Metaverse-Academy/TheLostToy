1dbb4d1fe6e49fbc5b6592fbe34173f98d149617
u-n79 <cooc.2010589@gmail.com>
2026-05-20 02:28:19 +0300

    I fixed all the bugs

--- Assets/Folder/Scripts/AliScripts/PlayerScripts/PlayerMovement3.cs
+++ Assets/Folder/Scripts/AliScripts/PlayerScripts/PlayerMovement3.cs
@@ -7,32 +7,25 @@ public class PlayerMovement3 : MonoBehaviour
     [SerializeField] float jumpForce = 7f;
     [SerializeField] LayerMask groundLayer;
     [SerializeField] float groundCheckDistance = 1.1f;
-
     private Rigidbody rb;
     private Vector2 moveInput;
     private bool isGrounded;
-
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
-
     void Update()
     {
         isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundLayer);
-
     }
-
     void FixedUpdate()
     {
         HandleMovement();
     }
-
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
-
     void OnJump(InputValue value)
     {
         if (value.isPressed && isGrounded)
@@ -40,18 +33,15 @@ public class PlayerMovement3 : MonoBehaviour
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
-
     void HandleMovement()
     {
         Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
         move = transform.TransformDirection(move);
         rb.linearVelocity = new Vector3(move.x * moveSpeed, rb.linearVelocity.y, move.z * moveSpeed);
     }
-
     void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundCheckDistance);
     }
-
 }
